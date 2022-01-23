8df6600fd045fb428f54ba75228581f5c9a47bde
Vinter Andrada <vinterandrada08@gmail.com>
2022-01-23 12:41:42 +0200

    Add project files.

---
+++ 4 numere in ordine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_numere_in_ordine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
